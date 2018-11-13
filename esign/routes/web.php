3846b2c9ac46f270a4fa0c8293a0bb1b7a5700e2
Vijay Tandel <tandelvijay19@gmail.com>
2018-11-13 20:05:30 +0530

    Completed the welcome page and home is WIP

--- esign/routes/web.php
+++ esign/routes/web.php
@@ -17,4 +17,14 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/home', 'HomeController@index');
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+
+//Route::group(['prefix' => 'admin',  'middleware' => 'auth:admin'], function() {
+//    Route::get('home','HomeController@index')->name('home');
+//    Route::post('logout','HomeController@logout')->name('admin.logout');
+//});
