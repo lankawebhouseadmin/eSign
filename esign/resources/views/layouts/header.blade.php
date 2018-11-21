52d09eefee385e40c5fdddbc7081cd285acda599
Sohan Tandel <sohantandel@outlook.com>
2018-11-21 17:42:32 +0530

    Html design myfiles WIP

--- esign/resources/views/layouts/header.blade.php
+++ esign/resources/views/layouts/header.blade.php
@@ -1,60 +1,55 @@
-<header class="main-header">
-  <!-- Logo -->
-  <a href="{{ route('home') }}" class="logo">
-    <!-- mini logo for sidebar mini 50x50 pixels -->
-    <span class="logo-mini"><b>eSign</b></span>
-    <!-- logo for regular state and mobile devices -->
-    <span class="logo-lg"><b>eSign</b></span>
-  </a>
-  <!-- Header Navbar: style can be found in header.less -->
-  <nav class="navbar navbar-static-top navbar-clr">
-    <!-- Sidebar toggle button-->
-    <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-      <span class="sr-only">Toggle navigation</span>
-    </a>
-
-    <div class="navbar-custom-menu">
-      <ul class="nav navbar-nav">
-        <!-- Messages: style can be found in dropdown.less-->
-        <li class="dropdown user user-menu">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-            <img src="{{ asset('assets/image/avatar.png') }}" class="user-image" alt="Admin Image">
-            <span class="hidden-xs">{{ Auth::user()->name }}</span>
-          </a>
-          <ul class="dropdown-menu">
-            <!-- User image -->
-            <li class="user-header">
-              <img src="{{ asset('assets/image/avatar.png') }}" class="img-circle" alt="Admin Image">
-
-              <p>
-                {{ Auth::user()->name }}
-              </p>
-            </li>
-            <!-- Menu Body -->
-
-            <!-- Menu Footer-->
-            <li class="user-footer">
-              {{--<div class="pull-left">
-                <a href="#" class="btn btn-default btn-flat">Profile</a>
-              </div>--}}
-              <div class="pull-right">
-                {{--<a href="#" class="btn btn-default btn-flat">Sign out</a>--}}
-                <a href="{{ route('logout') }}" class="btn btn-default btn-flat"
-                   onclick="event.preventDefault();
-                   document.getElementById('logout-form').submit();">
-                  Logout
-                </a>
-
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                  {{ csrf_field() }}
-                </form>
-              </div>
-            </li>
-          </ul>
-        </li>
-        <!-- Control Sidebar Toggle Button -->
-
-      </ul>
+<?php
+    // @TODO:: this is temp solution due to lack of time, please find with valid standard
+    $fullUrl = Request::path();
+    $subUrl = explode('/', $fullUrl);
+    $url = $subUrl[0];
+?>
+<div class="header-wrapper">
+    <div class="brand-header {{ $url == 'home' ? 'd-none' : '' }}">
+        <a href="{{ route('home') }}"><img class="mt-3" src="{{ asset('assets/image/esign-logo.png') }}"></a>
     </div>
-  </nav>
-</header>
+    <div class="text-right px-3 pt-3">
+        <a href="{{ route('logout') }}"
+           onclick="event.preventDefault();
+                   document.getElementById('logout-form').submit();"
+           class="btn access-button main-btn logout font-blue-light-14">Log Out</a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            {{ csrf_field() }}
+        </form>
+    </div>
+    <div class="header text-right">
+        <div class="list-inline">
+            {{--<div class="list-inline-item">--}}
+            {{--<img class="icon-head"  src="{{ asset('assets/image/home-icon.png') }}">--}}
+            {{--<img class="ml-3 icon-head"  src="{{ asset('assets/image/us.png') }}">--}}
+            {{--</div>--}}
+            <div class="list-inline-item">
+                {{--HOME MENU--}}
+                <div class="menu-wrapper">
+                    <a href="{{ route('home') }}" class="circle">
+                        <img class="icon-head" src="{{ asset('assets/image/home.png') }}">
+                    </a>
+                    <div class="menu-text">Home</div>
+                </div>
+
+                {{--HOME MENU--}}
+                <div class="menu-wrapper">
+                    <a href="{{ route('myfiles') }}" class="circle">
+                        <img class="icon-head" src="{{ asset('assets/image/home.png') }}">
+                    </a>
+                    <div class="menu-text">My Files</div>
+                </div>
+                {{--PROFILE MENU--}}
+                <div class="menu-wrapper">
+                    <a href="#" class="circle">
+                        <img class="icon-head" src="{{ asset('assets/image/profile.png') }}">
+                    </a>
+                    <div class="menu-text">Profile</div>
+                </div>
+            </div>
+            <div class="list-inline-item time-container">
+                <span class="mr-3 font-blue-light-14">01/01/2018 12:00 PM</span>
+            </div>
+        </div>
+    </div>
+</div>
